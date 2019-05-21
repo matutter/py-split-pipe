87576290cfa5915120e2c5de9d442411240546c4
matutter <matutter4@gmail.com>
2019-05-21 19:16:09 -0400

    fixed `buffered` kwarg logic

--- tee.py
+++ tee.py
@@ -16,15 +16,21 @@ def clean(*args):
 # will copy to other file if kwarg `filename` is specified
 # and will cleanup tempfile
 def exec_with_split(cmd, **kwargs):
+  """
+  @kwarg filename - Will copy output to a filename
+  @kwarg tail - Will print output to stdout while subprocess is running.
+  @kwarg no_ascii - Will strip ASCII escape sequences, including color codes, from the `filename`.
+  @kwarg buffered - If falsy will pass `cmd` as arguments to stdbuf and run `cmd` with unbuffered output.
+  """
   filename = kwargs.get('filename', None)
   tail     = kwargs.get('tail', True)
   no_ascii = kwargs.get("no_ascii", False)
-  buffered = kwargs.get("buffered", False)
+  buffered = kwargs.get("buffered", True)
   retcode  = None
   co_procs = []
   wrapper_cmd = []
 
-  if buffered:
+  if not buffered:
     wrapper_cmd.extend(["stdbuf", "-oL"])
 
   with tempfile.NamedTemporaryFile("wb") as fd:
@@ -51,7 +57,7 @@ if __name__ == "__main__":
   clean('mycopy*')
   cmd = ['python', 'runme.py']
   print("Run with tail")
-  exec_with_split(cmd, filename="mycopy.txt", buffered=True)
+  exec_with_split(cmd, filename="mycopy.txt", buffered=False)
 
   print("Run no tail")
   exec_with_split(cmd, filename="mycopy2.txt", tail=False, no_ascii=True)
